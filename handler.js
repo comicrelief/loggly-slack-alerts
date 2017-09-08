37d2e7a96ce410f7cbc6e58e7c124e4c642fb6a4
John Levermore <john.levermore@zhibek.com>
2017-09-08 08:59:52 +0100

    Adapt boiler plate to create single "/alert" endpoint, accessible via POST (remove redundant comments).

--- handler.js
+++ handler.js
@@ -1,6 +1,6 @@
 'use strict';
 
-module.exports.hello = (event, context, callback) => {
+module.exports.alert = (event, context, callback) => {
   const response = {
     statusCode: 200,
     body: JSON.stringify({
@@ -11,6 +11,4 @@ module.exports.hello = (event, context, callback) => {
 
   callback(null, response);
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
 };
